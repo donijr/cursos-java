4eae1db3c019dc66efa14ee56e4bb3aa64f2055a
donijr <donizetigmjr@gmail.com>
2021-02-21 20:42:38 -0300

    Arquivos adicionados

---
+++ devmedia/HelloWorld/src/HelloWord.java
@@ -0,0 +1,7 @@
+public class HelloWord {
+
+    public void sayHello(){
+        System.out.println("Hello World");
+
+    }
+}
---
+++ devmedia/HelloWorld/src/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args){
+        HelloWord helloWord = new HelloWord();
+        helloWord.sayHello();
+
+    }
+}
